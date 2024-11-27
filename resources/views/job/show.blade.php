9140dffa057ad8a28c71d747edf8706a5855ae89
alfandi rifaul <125331290+alfandirifaul@users.noreply.github.com>
2024-11-27 16:41:16 +0700

    add other jobs from same company on one page

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -11,4 +11,30 @@
         </p>
     </x-job-card>
 
+    <x-card class="mb-4 ">
+        <h2 class="mb-4 text-lg font-medium">
+            More for {{ $job->employer->company_name }} Jobs
+        </h2>
+
+        <div class="text-sm text-slate-500">
+            @foreach($job->employer->jobs->filter(fn($otherJob) => $otherJob->id !== $job->id) as $otherJob)
+                <div class="mb-4 flex justify-between">
+                    <div>
+                        <div class="text-slate-700 hover:text-slate-500 hover:underline">
+                            <a href="{{ route('jobs.show', $otherJob) }}">
+                                {{ $otherJob->title }}
+                            </a>
+                        </div>
+                        <div class="text-xs">
+                            {{ $otherJob->created_at->diffForHumans() }}
+                        </div>
+                    </div>
+                    <div class="text-xs">
+                        ${{ number_format($otherJob->salary) }}
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </x-card>
+
 </x-layout>
